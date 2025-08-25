eb05a10a204072fefcdc548c984ed33ad8a4894b
NityaPotti <126093831+NityaPotti@users.noreply.github.com>
2025-08-24 22:28:25 -0400

    Add files via upload

---
+++ src/components/education.js
@@ -0,0 +1,19 @@
+export const education = [ //array. The reason we use export keyword is so that this array is usable in other js files
+{
+   id: 1,
+   title : "Georgia Institute of Technology",
+   date : "Aug 2023-present",
+   description: "Computer Science Major with Threads in Artifical Intelligence and Info-Internetworks",
+   activities : "The Women's Network (President), IOS Club (Tech Lead), GITMAD (Tech Lead) club, Video Game Development Club, Dazzlers Dance Team (Founder)",
+   icon : "fa-solid fa-building-columns"
+},
+{ //array of objects. { are used to define pbject literals (new object with properties and values already initialized)
+    id: 2,
+    title : "Eden Prairie High School : MN",
+    date : "Sept 2019-May 2023",
+    description: "4.0 UW GPA, AP Scholar with Distinction",
+    activities: "School's National Speech and Debate Captain, Founder of EPHS's USA Computing Olympics Organization, President of High School Tutoring Organization, Founder of Ink and Fable Reading and Writing Club and Captain of Middle School Tutoring Organization",
+    icon : "fa-solid fa-school"
+ }
+]
+export default education;
---
+++ src/routes/Photo.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+function Photo() {
+    return(
+        <>
+        <Navbar />
+        <h1>Photo Journal</h1>
+        </>
+    )
+}
+export default Photo;
